ac14d1d05f7848a574893e700e56c0bdc9ca092c
JebinDanielR <jebinderil@gmail.com>
2026-07-13 21:00:49 +0530

    Added edit button to each Card

--- capstone-react/src/Components/Card.jsx
+++ capstone-react/src/Components/Card.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
+
 function Card({ task }) {
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState(task.title);
+  const [description, setDescription] = useState(task.description);
 
   return (
     <div className="card">
+        
+      {
+        isEditing ? (
+          <form>
+            <input
+              type="text"
+              value={title}
+              onChange={(e)=>setTitle(e.target.value)}/>
 
-      <h3>{task.title}</h3>
+            <textarea
+              value={description}
+              onChange={(e)=>setDescription(e.target.value)}/>
 
-      <p>{task.description}</p>
+            <button type="button">
+              Save
+            </button>
+          </form>
+          ) :(
+          <>
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
+            <button onClick={()=>setIsEditing(true)}>Edit</button>
+          </>
+        )
+      }
 
     </div>
   );
