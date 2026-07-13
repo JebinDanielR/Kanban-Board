8a5af65ff3beb03cf2e6edac8ff96275216cad74
JebinDanielR <jebinderil@gmail.com>
2026-07-13 19:56:51 +0530

    Controlled Form

--- capstone-react/src/Components/TaskForm.jsx
+++ capstone-react/src/Components/TaskForm.jsx
@@ -1,21 +1,95 @@
-function TaskForm(){
+import { useState } from "react";
 
-  return(
-    <div className="task-form">
+function TaskForm({ addTask, columns }) {
 
-      <input 
-        placeholder="Task title"
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [column, setColumn] = useState("todo");
+
+  const [error, setError] = useState("");
+
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+
+
+    if(title.trim() === "") {
+      setError("Title is required");
+      return;
+    }
+
+
+    const newTask = {
+      id: crypto.randomUUID(),
+      title,
+      description,
+      column
+    };
+
+
+    addTask(newTask);
+
+
+    // clear form
+    setTitle("");
+    setDescription("");
+    setColumn("todo");
+    setError("");
+
+  };
+
+
+  return (
+
+    <form onSubmit={handleSubmit}>
+
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e)=>setTitle(e.target.value)}
       />
+
 
       <textarea
-        placeholder="Task description"
+        placeholder="Description"
+        value={description}
+        onChange={(e)=>setDescription(e.target.value)}
       />
 
-      <button>
-        Add Task
+
+      <select
+        value={column}
+        onChange={(e)=>setColumn(e.target.value)}
+      >
+
+        {
+          columns.map(col => (
+            <option 
+              key={col.id}
+              value={col.id}
+            >
+              {col.title}
+            </option>
+          ))
+        }
+
+      </select>
+
+
+      <button type="submit">
+        Add Card
       </button>
 
-    </div>
+
+      {
+        error && <p>{error}</p>
+      }
+
+
+    </form>
+
   );
 
 }
